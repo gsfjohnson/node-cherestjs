6267f39a9cb0d194a0f5781d755bff024f488a6c
Travis Payton <tpayton1@alaska.edu>
2017-06-07 23:50:49 -0800

    feat: Adds the ability to perform a search by providing a search_obj. Also adds a basic search (getOpenTicketsTeam) for listing all tickets with Status of New or In Progress and OwnedByTeam of provided team name.

--- index.js
+++ index.js
@@ -78,6 +78,63 @@ Cherestjs.prototype.getBusinessObject = function (id, callback) {
 
 };
 
+Cherestjs.prototype.search = function(search_obj, callback){
+  var self = this;
+    self.getToken(function (err, token_key) {
+        if (err) { callback(err, null); return; }
+
+        var url = 'https://' + self.host + self.v1_endpoint + '/getsearchresults';
+
+        var opts = {
+            timeout: 300000, // 5 minutes as Cherwell SaaS is slooooooooooooow to start sometimes.
+            accept: "application/json",
+            headers: { Authorization: "Bearer " + token_key,
+                       "Content-Type": "application/json"
+                     }
+        };
+        needle.post(url, JSON.stringify(search_obj), opts, function (err, res) {
+            if (err) { callback(err, null); return; }
+            if (res.parser!='json') { callback('invalid response parser', null); return; }
+
+            var tickets = []
+            for (var t=0; t < res.body.businessObjects.length; t++){
+              var fields = {};
+              var data = res.body.businessObjects[t].fields;
+              for (var i=0; i < data.length; i++) {
+                  fields[ data[i]["name"] ] = data[i]["value"];
+              }
+              tickets.push(fields)
+            }
+            callback(false, tickets);
+        });
+
+    });
+}
+
+Cherestjs.prototype.getOpenTicketsTeam = function(team_name, callback) {
+  var self = this;
+  var search_obj = {
+      "filters": [
+          {
+              "fieldId": "BO:6dd53665c0c24cab86870a21cf6434ae,FI:9339fc404e8d5299b7a7c64de79ab81a1c1ff4306c",
+              "operator": "eq",
+              "value": team_name
+          },
+          {
+              "fieldId": "BO:6dd53665c0c24cab86870a21cf6434ae,FI:5eb3234ae1344c64a19819eda437f18d",
+              "operator": "eq",
+              "value": "New"
+          },
+          {
+              "fieldId": "BO:6dd53665c0c24cab86870a21cf6434ae,FI:5eb3234ae1344c64a19819eda437f18d",
+              "operator": "eq",
+              "value": "In Progress"
+          }
+      ],
+
+      "busObId": "6dd53665c0c24cab86870a21cf6434ae",
+  }
+
 module.exports = function (host, apikey, username, password) {
   return new Cherestjs(host, apikey, username, password);
 };
